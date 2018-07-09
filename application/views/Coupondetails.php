a4c27a8a793fc6b5f707b3cce7085c980f7edbd0
pranshuTyagi <tyagi.pranshu13@gmail.com>
2018-07-09 20:47:58 +0530

    Add files via upload
    CouponDetails Forms
    and Data from database table

---
+++ application/views/Coupondetails.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Coupon Details</title>
+</head>
+
+<body>
+	<form method="post">
+		<table width="600" border="1" cellspacing="5" cellpadding="5">
+  <tr>
+    <td width="230">Enter CouponCode </td>
+    <td width="329"><input type="text" name="CouponCode"/></td>
+  </tr>
+  <tr>
+    <td>Enter Your CouponType </td>
+    <td><input type="text" name="CouponType"/></td>
+  </tr>
+  <tr>
+    <td>Enter Your CouponValidity </td>
+    <td><input type="date_time_set" name="CouponValidity"/></td>
+  </tr>
+  <tr>
+    <td colspan="2" align="center"><input type="submit" name="save" value="Save Data"/></td>
+  </tr>
+</table>
+
+	</form>
+</body>
+</html>
---
+++ application/views/display_records.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Display Records</title>
+</head>
+
+<body>
+  <?php
+  $i=1;
+  foreach($data as $row)
+  
+  
+ 
+   ?>
+
+
+</body>
+</html>
